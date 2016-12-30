e48ef5c1a2301571c2d7182e6406a7483e2c9ecc
LaCruz <lacruz@gmx.net>
2016-12-30 05:02:44 +0100

    common version - works

---
+++ BoardConfig.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/n2awifi
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_SOC := exynos5420
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Bootloader
+TARGET_OTA_ASSERT_DEVICE := picassowifi,n2awifi
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_deathly_n2awifi_defconfig
+
+# Includes
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Hardware
+BOARD_HARDWARE_CLASS += $(LOCAL_PATH)/cmhw
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2527068160
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12532580352
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
+
+# Inherit from exynos5420-common
+include device/samsung/exynos5420-common/BoardConfigCommon.mk
